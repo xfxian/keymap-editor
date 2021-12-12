4db4372283f93eb420a43e3ad16330caf6ab6072
panzerstadt <tliqun@gmail.com>
2021-12-12 17:21:57 +0900

    lint + quickhack to always load local keyboard

--- application/components/keyboard-picker.vue
+++ application/components/keyboard-picker.vue
@@ -1,85 +1,89 @@
 <template>
-  <div>
-    <selector
+  <div id="spacer">
+    <!-- <selector
       v-model="source"
       label="Source"
       :id="source"
       :choices="sourceChoices"
     />
 
-    <github-picker
-      v-if="source == 'github'"
-      @select="handleKeyboardSelected"
-    />
+    <github-picker v-if="source == 'github'" @select="handleKeyboardSelected" /> -->
   </div>
 </template>
 
 <script>
-import compact from 'lodash/compact'
+import compact from "lodash/compact";
 
-import * as config from '../config'
-import { loadLayout } from '../layout.js'
-import { loadKeymap } from '../keymap.js'
+import * as config from "../config";
+import { loadLayout } from "../layout.js";
+import { loadKeymap } from "../keymap.js";
 
-import GithubPicker from './github/picker.vue'
-import Selector from './selector.vue'
+import GithubPicker from "./github/picker.vue";
+import Selector from "./selector.vue";
 
 export default {
-  name: 'KeyboardPicker',
+  name: "KeyboardPicker",
   components: { GithubPicker, Selector },
-  emits: ['select'],
+  emits: ["select"],
   data() {
     const sourceChoices = compact([
-      config.enableLocal ? { id: 'local', name: 'Local' } : null,
-      config.enableGitHub ? { id: 'github', name: 'GitHub' } : null
-    ])
+      config.enableLocal ? { id: "local", name: "Local" } : null,
+      config.enableGitHub ? { id: "github", name: "GitHub" } : null,
+    ]);
 
-    const selectedSource = localStorage.getItem('selectedSource')
-    const onlySource = sourceChoices.length === 1 ? sourceChoices[0].id : null
+    const selectedSource = localStorage.getItem("selectedSource");
+    const onlySource = sourceChoices.length === 1 ? sourceChoices[0].id : null;
 
     return {
       sourceChoices,
-      source: onlySource || (
-        sourceChoices.find(source => source.id === selectedSource)
+      source:
+        onlySource ||
+        (sourceChoices.find((source) => source.id === selectedSource)
           ? selectedSource.id
-          : null
-      )
-    }
+          : null),
+    };
   },
   mounted() {
-    if (this.source === 'local') {
-      this.fetchLocalKeyboard()
-    }
+    this.fetchLocalKeyboard();
+    // TODO: make this fetch differnt local keyboards
+    // if (this.source === "local") {
+    //   this.fetchLocalKeyboard();
+    // }
   },
   watch: {
     source(value) {
-      localStorage.setItem('selectedSource', value)
-      if (value === 'local') {
-        this.fetchLocalKeyboard()
+      localStorage.setItem("selectedSource", value);
+      if (value === "local") {
+        this.fetchLocalKeyboard();
       }
-    }
+    },
   },
   methods: {
     async fetchLocalKeyboard() {
-      const { source } = this
-      const [layout, keymap] = await Promise.all([
-        loadLayout(),
-        loadKeymap()
-      ])
+      const { source } = this;
+      // pass differnt keyboard layout options here
+      const [layout, keymap] = await Promise.all([loadLayout(), loadKeymap()]);
 
-      this.handleKeyboardSelected({ source, layout, keymap })
+      this.handleKeyboardSelected({ source, layout, keymap });
     },
     handleKeyboardSelected(event) {
-      const { source } = this
-      const { layout, keymap, ...rest } = event
+      const { source } = this;
+      const { layout, keymap, ...rest } = event;
 
-      const layerNames = keymap.layer_names || keymap.layers.map((_, i) => `Layer ${i}`)
+      const layerNames =
+        keymap.layer_names || keymap.layers.map((_, i) => `Layer ${i}`);
       Object.assign(keymap, {
-        layer_names: layerNames
-      })
+        layer_names: layerNames,
+      });
 
-      this.$emit('select', { source, layout, keymap, ...rest })
-    }
-  }
-}
+      this.$emit("select", { source, layout, keymap, ...rest });
+    },
+  },
+};
 </script>
+
+<style scoped>
+#spacer {
+  height: 20px;
+}
+</style>
