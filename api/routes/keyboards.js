48dea615a0fc4ffe1e08636066214f3baec4c58c
panzerstadt <tliqun@gmail.com>
2021-12-21 22:51:21 +0900

    teeny workarounds

--- api/routes/keyboards.js
+++ api/routes/keyboards.js
@@ -27,4 +27,17 @@ router.post('/keymap', (req, res) => {
   // })
 })
 
+// this just allows me to match what the pw site has, since the vue part is basically the same as
+// what's going to be on the site
+router.post('/api/editor/build', (req, res) => res.redirect(307, '/api/editor/keymap'))
+router.post("/api/editor/keymap", (req, res) => {
+  if (req.method === 'POST') {
+    const keymap = req.body
+    const layout = zmk.loadLayout()
+    const generatedKeymap = zmk.generateKeymap(layout, keymap)
+
+    return res.status(200).json(generatedKeymap.json)
+  }
+})
+
 module.exports = router
--- application/config.js
+++ application/config.js
@@ -1,6 +1,7 @@
 
 
-export const apiBaseUrl = "/api/editor"
+// export const apiBaseUrl = "/api/editor" // this is for thw pw website
+export const apiBaseUrl = "http://localhost:8080" // this points to the api server instead
 export const appBaseUrl = "/editor/index.html"
 export const githubAppName = ""
 export const enableGitHub = false
